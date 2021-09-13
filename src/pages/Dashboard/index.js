74a215c0bc628c05422bf63c479e6c61b3f6a7ea
Daniel <danielpreis@uol.com.br>
2021-09-13 09:19:21 -0300

    fix dashboar query

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -16,33 +16,16 @@ const Dashboard = () => {
   const { t } = useTranslation()
   const dispatch = useDispatch()
   const { data, success, error } = useSelector((state) => state.Dashboards)
-  const orderData = objToArray(data).sort(
-    (a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
-  )
-  const series = [
-    {
-      name: 'Cases',
-      data: [
-        555,
-        12038,
-        69030,
-        88369,
-        167466,
-        932638,
-        2055423,
-        3343777,
-        3845718
-      ]
-    },
-    {
-      name: 'Recovered',
-      data: [28, 284, 9394, 42710, 76026, 191853, 501538, 1029651, 1255481]
-    },
-    {
-      name: 'Deaths',
-      data: [17, 259, 1666, 2996, 6472, 49675, 140658, 238619, 269567]
-    }
-  ]
+
+  const arrayData = objToArray(data)
+  console.log('arrayData', arrayData)
+
+  const series = arrayData.map((e) => ({
+    name: e.ovitrap_name,
+    data: e?.data?.map((e2) => [e2.created_at, e2.number]) ?? []
+  }))
+  console.log('series', series)
+
   const options = {
     dataLabels: {
       enabled: false
@@ -51,14 +34,14 @@ const Dashboard = () => {
       curve: 'smooth'
     },
     xaxis: {
-      type: 'category',
-      categories: orderData.map((e) =>
-        format(parseISO(e.createdAt), 'dd/MM/yy')
-      )
+      type: 'datetime'
+    },
+    yaxis: {
+      min: 0
     },
     tooltip: {
       x: {
-        format: 'dd/MM/yy'
+        format: 'hh:mm:ss dd/MM/yy'
       }
     }
   }
@@ -73,39 +56,16 @@ const Dashboard = () => {
   return (
     <div>
       <Grid container spacing={3}>
-        <Grid item xs={12} md={8} lg={9}>
-          <Paper
-            sx={{
-              p: 2,
-              display: 'flex',
-              flexDirection: 'column',
-              height: 240
-            }}
-          >
-            <Charts
-              options={options}
-              series={series}
-              type="line"
-              height={240}
-            />
-          </Paper>
-        </Grid>
-        <Grid item xs={12} md={4} lg={3}>
-          <Paper
-            sx={{
-              p: 2,
-              display: 'flex',
-              flexDirection: 'column',
-              height: 240
-            }}
-          >
-            BBBB
+        <Grid item xs={6}>
+          <Paper>
+            <Charts options={options} series={series} type="line" />
           </Paper>
         </Grid>
         <Grid item xs={12}>
-          <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-            CCCCC
-          </Paper>
+          <Paper>BBBB</Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper>CCCCC</Paper>
         </Grid>
       </Grid>
       {(success || error) && (
